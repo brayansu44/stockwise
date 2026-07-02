604eee8e95c05297ce60a75ee0f3a7f2ae21bedb
brayansu44 <brayansayrez400@gmail.com>
2026-07-02 03:47:07 -0500

    Remove automatic database initialization from main

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from app.presentation.routers.health import router as health_router
 from app.presentation.routers.products import router as products_router
 from app.core.config import settings
-from app.infrastructure.database.init_db import init_db
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -13,8 +12,6 @@ app = FastAPI(
 app.include_router(health_router)
 
 app.include_router(products_router)
-
-init_db()
 
 @app.get("/")
 def read_root():
